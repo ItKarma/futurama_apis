7d1872d2c2ae3c2c14bdc60d5abade3db5d94dfb
danzok <loppesofc@gmail.com>
2022-12-11 18:01:18 -0300

    adding environment variables

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 export default async function connectionDb () {
   mongoose.set('strictQuery', true)
-  const db = mongoose.connect('mongodb+srv://Lopes01:Lopes01@cluster0.evxl2c8.mongodb.net/Userdb?retryWrites=true&w=majority')
+  const db = mongoose.connect(process.env.URL_DB)
 
   db.then(() => {
     console.log('Connected to database')
